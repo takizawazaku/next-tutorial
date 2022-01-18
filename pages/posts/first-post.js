e73f92f30cf789d8d1ccaf3d56d6092ab4e30320
takizawazaku <takizawazaku@gmail.com>
2022-01-18 22:21:20 +0900

    Assets, Metadata, and CSS Complete

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,11 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import Layout from '../../components/layout'
+
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>とらゼミFirst Post</title>
       </Head>
@@ -14,6 +16,6 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   )
 }
